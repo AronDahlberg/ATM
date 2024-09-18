e01ea25944c9e1e942c463db93c97b97075823ed
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-09-18 16:45:56 +0200

    Added ATM class

--- ATM/Program.cs
+++ ATM/Program.cs
@@ -2,44 +2,15 @@
 {
     internal class Program
     {
-        private static string ClearConsole { get; } = "\x1b[2J\x1b[H"; // ANSI ESC Code
-        private static bool IsRunning { get; set; } = true;
         static void Main(string[] args)
         {
             string currency = "kr";
 
             var accounts = PopulatedAccounts();
 
-            Bank bank = new Bank(accounts, currency);
+            ATM ATM = new ATM(accounts, currency);
 
-            string? input;
-
-            while (IsRunning)
-            {
-                Console.Write(ClearConsole +
-                        "0: Make a deposit\n" +
-                        "1: Make a withdrawal\n" +
-                        "2: See balance\n" +
-                        "3: See all accounts\n" +
-                        "4: Exit\n");
-
-                input = Console.ReadLine();
-
-                switch (input)
-                {
-                    case "0": bank.Deposit(); break;
-
-                    case "1": bank.Withdraw(); break;
-
-                    case "2": bank.PrintBalance(); break;
-
-                    case "3": bank.PrintAccounts(); break;
-
-                    case "4": IsRunning = false; break;
-                }
-            }
-
-            Console.Write(ClearConsole);
+            ATM.Run();
         }
         private static Account[] PopulatedAccounts()
         {
