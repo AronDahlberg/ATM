03ec3b0aec0db556de4e9bd439e244322167216f
Aron Dalhberg <aron.t.dahlberg@gmail.com>
2024-09-18 15:49:59 +0200

    Added Account class

---
+++ ATM/Account.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATM
+{
+    internal class Account
+    {
+		private double _balance;
+
+		public double Balance
+		{
+			get { return _balance; }
+			set { _balance = value >= 0.0
+					? value
+					: _balance; }
+		}
+        public string AccountNumber { get; }
+        public Account(string accountNumber)
+		{
+			AccountNumber = accountNumber;
+		}
+		public void Deposit (double amount)
+		{
+			if (amount < 0.0) { throw new ArgumentException("Invalid amount"); }
+			Balance += amount;
+		}
+		public void Withdraw (double amount)
+		{
+            if (amount < 0.0) { throw new ArgumentException("Invalid amount"); }
+            Balance -= amount;
+		}
+        public override string ToString()
+		{
+			return $"{AccountNumber}: {Balance}";
+		}
+    }
+}
